4a6f2b593fd4d7f84d2d60913f637d494844ea94
Aman Pratap Singh <56557379+Spider8019@users.noreply.github.com>
2023-12-23 12:09:31 +0530

    Asdfa

--- src/components/card/BookCard.js
+++ src/components/card/BookCard.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { Card } from 'primereact/card'
+import { Tag } from 'primereact/tag'
 
-const BookCard = ({ title, url, author, innerRef, preface, book_id }) => {
+const BookCard = ({ title, url, author, innerRef, preface, mrp, language,dis_price }) => {
   const [max, setMax] = useState(false)
   const header = (
     <div className="relative bg-gray-200">
@@ -18,18 +19,37 @@ const BookCard = ({ title, url, author, innerRef, preface, book_id }) => {
       />
     </div>
   )
+  const subTitle = (
+    <div>
+      <p>{author}</p>
+      <div>
+        <Tag
+          style={{ borderColor: '#006233', background: '#0062339F' }} 
+          className="border"
+        >
+          <s className='font-light text-xs'>₹{mrp}</s> ₹{dis_price}
+        </Tag>
+        <Tag
+          style={{ borderColor: '#006233', background: '#0048629F' }}
+          className="ml-2 border"
+        >
+          {language}
+        </Tag>
+      </div>
+    </div>
+  )
   return (
     <Card
       layout
       role="region"
       ref={innerRef}
       title={title}
-      subTitle={author}
+      subTitle={subTitle}
       header={header}
       className="mx-4 h-full"
     >
       <div className="">
-        <p className="text-sm line-clamp-5">{preface}</p>
+        <p className="text-sm line-clamp-3">{preface}</p>
       </div>
     </Card>
   )
